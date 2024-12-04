8fad19c6f0be132b91f94775dbe09ceb95e0b219
vivek Aryan <164575024+vivek2024IT@users.noreply.github.com>
2024-12-05 00:41:36 +0530

    Create CategoryController.java

---
+++ CategoryController.java
@@ -0,0 +1,43 @@
+package com.Nimap.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;  // <-- Add this import
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public Page<Category> getAllCategories(Pageable pageable) {
+        return categoryService.getAllCategories(pageable);
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+    
+  
+}
+
+
