279c2eb7a3ed571a77cfe79e15360a54b8057e72
vivek Aryan <164575024+vivek2024IT@users.noreply.github.com>
2024-12-05 00:35:09 +0530

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,43 @@
+package com.Nimap.Test;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Product> products;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
