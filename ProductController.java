76b3fc4f95d512fbc813f2904c582da2a0af0ada
vivek Aryan <164575024+vivek2024IT@users.noreply.github.com>
2024-12-05 00:47:59 +0530

    Create ProductController.java

---
+++ ProductController.java
@@ -0,0 +1,39 @@
+package com.Nimap.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/{categoryId}")
+    public Product createProduct(@RequestBody Product product, @PathVariable Long categoryId) {
+        return productService.createProduct(product, categoryId);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
